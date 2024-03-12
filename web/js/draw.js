de619eca9e82e358b65effce87f9aec5b1f6779c
Ebo Arcadia <yiboli.coding@gmail.com>
2024-03-12 12:04:49 -0400

    sketch pad

---
+++ web/js/draw.js
@@ -0,0 +1,20 @@
+draw = {};
+
+draw.path = (context, path, color = "black") => {
+  context.strokeStyle = color;
+  context.lineWidth = 3;
+  context.beginPath();
+  context.moveTo(...path[0]);
+  for (let i = 1; i < path.length; i++) {
+    context.lineTo(...path[i]);
+  }
+  context.lineCap = "round";
+  context.lineJoin = "round";
+  context.stroke();
+};
+
+draw.paths = (context, paths, color = "black") => {
+  for (const path of paths) {
+    draw.path(context, path, color);
+  }
+};
